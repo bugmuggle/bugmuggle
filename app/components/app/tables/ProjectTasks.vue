c56d59f41399c618902bb1d41e24d37c4487e902
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2025-11-26 16:27:18 +0530

    feat: add projects table

---
+++ app/components/app/tables/ProjectTasks.vue
@@ -0,0 +1,26 @@
+<template>
+  <UTable :data="tasks" :columns="columns" :sort="defaultSort" class="w-full" />
+</template>
+
+<script setup>
+const props = defineProps({
+  tasks: { type: Array, required: true }
+})
+
+const columns = ref([
+  { accessorKey: 'title', header: 'Title', sortable: true },
+  {
+    accessorKey: 'assignee',
+    header: 'Assigned to',
+    cell: (ctx) => `User #${ctx.getValue()}`
+  },
+  { accessorKey: 'status', header: 'Status', sortable: true },
+  { accessorKey: 'deadline', header: 'Deadline', sortable: true },
+  {
+    id: 'actions',
+    header: 'Actions',
+    cell: () => h('div', [h('button', { class: 'u-button' }, 'View')])
+  }
+])
+const defaultSort = ref({ column: 'title', direction: 'asc' })
+</script>
