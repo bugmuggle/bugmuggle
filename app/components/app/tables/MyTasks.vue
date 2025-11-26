35eb37c37a48bde552d320b606c850e50fe6687f
kandalgaonkarshubham <kandalgaonkarshubham@gmail.com>
2025-11-26 16:00:39 +0530

    feat:; add my tasks table

---
+++ app/components/app/tables/MyTasks.vue
@@ -0,0 +1,21 @@
+<template>
+  <UTable :data="tasks" :columns="columns" :sort="defaultSort" class="w-full" />
+</template>
+
+<script setup>
+const props = defineProps({
+  tasks: { type: Array, required: true }
+})
+
+const columns = ref([
+  { accessorKey: 'title', header: 'Title', sortable: true },
+  { accessorKey: 'deadline', header: 'Deadline', sortable: true },
+  { accessorKey: 'status', header: 'Status', sortable: true },
+  {
+    id: 'actions',
+    header: 'Actions',
+    cell: () => h('div', [h('button', { class: 'u-button' }, 'Edit')])
+  }
+])
+const defaultSort = ref({ column: 'deadline', direction: 'asc' })
+</script>
